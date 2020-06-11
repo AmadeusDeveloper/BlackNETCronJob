cdfb48b84375a508b59b129245627014cfa45346
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-11 20:00:47 +0500

    Update cron.py

--- cron.py
+++ cron.py
@@ -1,14 +1,11 @@
 import os
 import time
 import requests
+print('if you need multiple requests separate them with commas')
 ping = input('Enter ping.php location: ')
-remove = input('Enter remove.php location: ')
 requests.get(ping)
-requests.get(remove)
 while True:
 	time.sleep(15)
 	requests.get(ping)
 	print('ping command send')
-	requests.get(remove)
-	print('remove command send')
 	
